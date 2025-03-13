2b5cb640465b707c6a5e34eebc3b04d5b98e42da
TiagoPaulino <tiago.paulino96@gmail.com>
2025-03-13 00:58:48 -0300

    Adiciona componentes ProductCard.vue e ProductGrid.vue para exibição de produtos

---
+++ src/components/product/ProductGrid.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+    <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-4 gap-4 align-center py-20">
+        <slot></slot>
+    </div>
+</template>
